b963719b17da7df72a86c06318b82ebf8b661bc2
Lazee222 <lanceterrazola8@gmail.com>
2026-07-23 23:36:36 +0800

    Aliah Interactive Library Kiosk and Cafeteria Queue Assistant

---
+++ LabActivity2.py
@@ -0,0 +1,42 @@
+def check_fast_lane(minutes_left, items, teacher_pass):
+
+    if teacher_pass:
+        return "Fast lane approved"
+
+    elif minutes_left < 10 and items <= 3:
+        return "Fast lane approved"
+
+    else:
+        return "Use regular line"
+
+
+students_in_line = [
+    {"name": "Marco", "minutes_left": 8, "items": 2, "teacher_pass": False},
+    {"name": "Diane", "minutes_left": 15, "items": 1, "teacher_pass": False},
+    {"name": "Kyle", "minutes_left": 5, "items": 6, "teacher_pass": False},
+    {"name": "Ella", "minutes_left": 20, "items": 5, "teacher_pass": True},
+]
+
+fast_lane_count = 0
+
+for student in students_in_line:
+
+    result = check_fast_lane(
+        student["minutes_left"],
+        student["items"],
+        student["teacher_pass"]
+    )
+
+    print(student["name"] + ":", result)
+
+    if result == "Fast lane approved":
+        fast_lane_count += 1
+
+    else:
+        print("Minutes left:", student["minutes_left"])
+        print("Please use the regular line.")
+
+    print()
+
+print("Total students approved for fast lane:", fast_lane_count)
+
